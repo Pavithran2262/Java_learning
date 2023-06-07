b7fd151c58c3007d09eeb986f9ea1f6dda6c473d
pavithran <pavithran2262@gmail.com>
2023-06-07 12:12:50 +0530

    JPA practice with CRUD operation

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,7 @@ package org.example;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import java.util.List;
 
 /**
  * Hello world!
@@ -12,26 +13,55 @@ public class Main
 {
     public static void main( String[] args )
     {
-        demo a = new demo();
-        a.setId(11);
-        a.setName1("kain");
-        a.setTech("flutter");
-
-
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("pu");
         EntityManager em = emf.createEntityManager();
 
-
-        demo b = em.find(demo.class,1);
-//                                                      demo c = em.remove();
+        // Create operation
         em.getTransaction().begin();
-        demo d= em.find(demo.class,2);
-        em.remove(d);
-//        em.persist(b);
+        demo a = new demo();
+        a.setId(21);
+        a.setName1("kain");
+        a.setTech("flutter");
+        em.persist(a);
         em.getTransaction().commit();
 
-        System.out.println(a);
+        // Read operation
+        demo b = em.find(demo.class, 1);
         System.out.println(b);
+
+        // Update operation
+        em.getTransaction().begin();
+        demo c = em.find(demo.class, 9);
+        c.setName1("updatedName");
+        em.getTransaction().commit();
+
+        // Delete operation
+        em.getTransaction().begin();
+        demo d = em.find(demo.class, 3);
+        em.remove(d);
+        em.getTransaction().commit();
+
+        // Read all operation
+        em.getTransaction().begin();
+        List<demo> demos = em.createQuery("SELECT d FROM demo d", demo.class).getResultList();
+        for (demo demo : demos) {
+            System.out.println(demo);
+        }
+        em.getTransaction().commit();
+
+        em.close();
+        emf.close();
+        System.out.println("hi");
+//        demo b = em.find(demo.class,1);
+////                                                      demo c = em.remove();
+//        em.getTransaction().begin();
+//        demo d= em.find(demo.class,2);
+//        em.remove(d);
+////        em.persist(b);
+//        em.getTransaction().commit();
+//
+//        System.out.println(a);
+//        System.out.println(b);
 
     }
 }
